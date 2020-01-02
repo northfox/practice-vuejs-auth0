f25762a6e89c4621d29bf584bee04cd61232e683
northfox <vulpes.engineer@gmail.com>
2020-01-02 18:25:35 +0900

    Init & adding auth

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div v-if="!$auth.loading">
+      <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+      <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld'
+
+export default {
+  name: 'home',
+  compoments: {
+    HelloWorld
+  },
+  methods: {
+    login() {
+      this.$auth.loginWithRedirect()
+    },
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      })
+    }
+  }
+}
+</script>
